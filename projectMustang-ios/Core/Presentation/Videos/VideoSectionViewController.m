08b23c841b2343552e7fff9a27473efc14eaa6d0
Alan Hsu <alan.hsu@digiflare.com>
2015-06-24 13:30:16 -0400

    test video playback

--- projectMustang-ios/Core/Presentation/Videos/VideoSectionViewController.m
+++ projectMustang-ios/Core/Presentation/Videos/VideoSectionViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "VideoSectionViewController.h"
+@import MediaPlayer;
 
 @implementation VideoSectionViewController
 
@@ -15,5 +16,24 @@
     [super viewDidLoad];
     self.title = @"Videos";
     [self.view setBackgroundColor:[UIColor blueColor]];
+    
+    [self startVideoPlayback];
+    
+}
+
+- (void) startVideoPlayback {
+    
+    NSString* videoURLString = @"http://videos.contentful.com/1oq1lgnwupsh/Z64eJTxpq8OM4Ws20GckE/15fbbe2cb432a46f0941dd9dc556886b/Ariana_Grande_-_Break_Free_ft._Zedd.mp4";
+    
+    NSURL *videoURL = [NSURL URLWithString:videoURLString ];
+    
+    MPMoviePlayerViewController *movie = [[MPMoviePlayerViewController alloc] initWithContentURL:videoURL];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(playbackFinishedCallback:)
+                                                 name:MPMoviePlayerPlaybackDidFinishNotification
+                                               object:movie];
+    
+    [self presentMoviePlayerViewControllerAnimated:movie];
 }
 @end
